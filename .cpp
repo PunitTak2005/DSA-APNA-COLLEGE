941e0c009875f15f4133b41615270895a58c98b5
Punit Tak <147526916+PunitTak2005@users.noreply.github.com>
2025-03-30 14:29:28 +0530

    Create .cpp

---
+++ .cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a= 50;
+    int b= 10;
+    int q=a/b;
+    cout << "The quotient of " << a << " and " << b << " is: " << q << endl;
+    return 0;
+}
