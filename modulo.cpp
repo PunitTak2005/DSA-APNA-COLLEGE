fa2601cd3bd65bd814ba84793937be270ba7a1f0
Punit Tak <147526916+PunitTak2005@users.noreply.github.com>
2025-03-30 14:32:36 +0530

    Create modulo.cpp

---
+++ modulo.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a= 50;
+    int b= 10;
+    int modulo = a % b; // Modulo operator
+    cout << "Modulo of " << a << " % " << b << " = " << modulo << endl; // Output: 0
+    return 0;
+}
