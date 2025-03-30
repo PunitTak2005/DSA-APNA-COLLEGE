e8c1648a893eb6f06cddb1acf8b98274061daaa0
Punit Tak <147526916+PunitTak2005@users.noreply.github.com>
2025-03-30 12:07:56 +0530

    Create type cast.cpp

---
+++ type cast.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    double price = 100.99;
+
+    int newprice = int(price);  
+    cout << "The price is: " << newprice << endl; // Output: 100
+    return 0;
+}
