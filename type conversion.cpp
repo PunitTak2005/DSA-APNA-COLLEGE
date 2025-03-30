832322cfb54124fc17ea4d5ad8abcbc586d3777d
Punit Tak <147526916+PunitTak2005@users.noreply.github.com>
2025-03-30 12:04:12 +0530

    Create type conversion.cpp

---
+++ type conversion.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    char grade = 'B';
+    int value = grade;
+    cout << "The ASCII value of grade " << grade << " is: " << value << endl;
+    return 0;
+}
