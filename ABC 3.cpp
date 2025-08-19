cf0eea432e26a41dca43c89161e42e967054bff9
Punit Tak <147526916+PunitTak2005@users.noreply.github.com>
2025-08-19 21:48:25 +0530

    Create ABC 3.cpp

---
+++ ABC 3.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter n: ";
+    cin >> n;
+
+    char ch = 'A';  // starting alphabet
+
+    for (int i = 1; i <= n; i++) {     // rows
+        for (int j = 1; j <= i; j++) { // columns
+            cout << ch << " ";
+            ch++;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
